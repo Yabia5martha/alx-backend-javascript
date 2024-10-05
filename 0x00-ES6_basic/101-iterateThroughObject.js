49d6e9e69ec39e6fac7f06daad45a592c0f37e76
Martha Siiyabia Tong <marthasiiyatong@gmail.com>
2024-10-04 22:01:45 -0700

    0x00. ES6 Basics

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+xport default function iterateThroughObject(reportWithIterator) {
+    const employees = [];
+    for (const item of reportWithIterator) {
+      employees.push(item);
+    }
+    return employees.join(' | ');
+  }
